d78161ef4966826957c6774e8139e5690259961a
Nephtalia <nephtaliasny@gmail.com>
2020-10-02 15:13:48 +0200

    Commit do2

---
+++ do2-pc4ds.R
@@ -0,0 +1,9 @@
+## File: do2-pc4ds.r
+## Description: Devoir obligatoire num?ro 2 de Parallel Computing for Data Science
+## Date: 02/10/2020
+
+
+## 1?re variante: m?thode du rejet ####
+#On veut simuler par la m?thode de rejet, la densit? 
+#d'une loi uniforme (u,v)
+
